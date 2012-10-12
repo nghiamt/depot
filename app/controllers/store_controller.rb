a051ae5166d866f545432ccd4f48605d826e3c56
nghiamt <mtn_khtn@yahoo.com>
2012-10-13 01:49:56 +0900

    add file

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,5 @@
 class StoreController < ApplicationController
   def index
-    @product = Product.all
+    @products = Product.all
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,19 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, :presence => true
   validates :title, :uniqueness => true
   validates :price, :numericality => {:greate_than_or_equal_to => 0.01 }
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+    return true
+    else
+      errors.add(:base, 'Line Items present')
+    return false
+    end
+  end
 end
