fa0158612a0311eba7a2cd3de0591a7d2a935cf5
nghiamt <mtn_khtn@yahoo.com>
2012-10-13 21:39:04 +0900

    add file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
   has_many :line_items
+  has_many :order, :through => :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, :presence => true
   validates :title, :uniqueness => true
